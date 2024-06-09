72299488ed6ca40fedea3488d916acf9b8b3d926
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-06-09 16:22:35 -0300

    create crud of the UserController

--- src/main/java/com/bluefox/business/controller/UserController.java
+++ src/main/java/com/bluefox/business/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.bluefox.business.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bluefox.business.repository.UserRepository;
 import com.bluefox.business.model.User;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -26,9 +35,52 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User updateUser = user.get();
+
+            updateUser.setName(newUser.getName());
+            updateUser.setEmail(newUser.getEmail());
+            updateUser.setPassword(newUser.getPassword());
+            updateUser.setRole(newUser.getRole());
+            updateUser.setStatus(newUser.getStatus());
+            
+            userRepository.save(updateUser);
+
+            return ResponseEntity.ok(updateUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
     
 }
