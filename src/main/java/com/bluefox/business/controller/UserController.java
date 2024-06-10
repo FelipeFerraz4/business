37ed8b6761893d668a5f639385c7e12891655a8c
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-06-09 22:30:39 -0300

    modifere createUser for status http 201

--- src/main/java/com/bluefox/business/controller/UserController.java
+++ src/main/java/com/bluefox/business/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bluefox.business.repository.UserRepository;
 import com.bluefox.business.model.User;
@@ -12,16 +13,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
-
-
 
 @RestController
 @RequestMapping("/users")
@@ -45,10 +43,21 @@ public class UserController {
         }
     }
     
+    // @PostMapping
+    // public ResponseEntity<User> createUser(@RequestBody User user) throws URISyntaxException {
+    //     User savedUser = userService.save(user);
+    //     URI location = new URI("/api/users/" + savedUser.getId());
+    //     return ResponseEntity.created(location).body(savedUser);
+    // }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saveUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(saveUser.getId())
+                        .toUri();
+        return ResponseEntity.created(location).body(saveUser);
     }
 
     @PutMapping("/{id}")
