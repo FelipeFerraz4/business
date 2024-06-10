21d0da10aef3a288c4b3db0d0ee5f86976b7bdf2
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-06-09 23:03:13 -0300

    create interface userService

---
+++ src/main/java/com/bluefox/business/service/UserService.java
@@ -0,0 +1,23 @@
+package com.bluefox.business.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.bluefox.business.model.User;
+
+public interface UserService {
+    
+    List<User> findAllUsers();
+
+    List<User> findAllActiveUsers();
+
+    Optional<User> findUserById(Long id);
+
+    User saveUser(User user);
+
+    Optional<User> updateUser(Long id, User updateUser);
+
+    Optional<User> disableUser(Long id);
+
+    void deleteById(Long id);
+}
